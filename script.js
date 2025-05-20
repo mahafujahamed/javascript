fa090904da6a76dcf1d8ee25e095d6ff4f6bbd6f
mahafuj ahamed <mahafujahamed068@gmail.com>
2025-05-20 18:31:21 +0600

    added some js code

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function myName() {
     document.getElementById("name").innerHTML = "Hello, my name is Mahafuj ahamed";
 }
 
+function odd(){
+    let carName = "lamborghini";
+    document.getElementById("car").innerHTML = carName;
+}
+odd();
+
+function myFunction() {
+    const cars = ["Saab", "Volvo", "BMW"];
+    // change an element
+    cars[0] = "Toyota";
+    // Add cars
+    cars.push("Audi");
+    document.getElementById("list").innerHTML = cars;
+}
